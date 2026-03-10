60da94fd87847d5a7ba37aaaed4b3eb03d1e4066
Nehathukrul-3128 <nehathukrul31@gmail.com>
2026-03-10 09:05:16 +0000

    tuple

---
+++ python09/03/pattern2.py
@@ -0,0 +1,56 @@
+#left increment triangle
+# count=int(input("enter row count="))
+# for i in range(count):
+#     for j in range(i+1):
+#         print("*",end="")
+#     print()
+
+#left increment pyramid
+# count=int(input("enter row count="))
+# for i in range(count):
+#     for j in range(count,i,-1):
+#         print("*",end="")
+#     print()
+
+#right-increment pyramid
+# count=int(input("enter row count="))
+# for i in range(count):
+#     for k in range(count,i+1,-1):
+#         print(end=" ")
+
+#     for j in range(i+1):
+#         print("*",end="")
+#     print()
+
+#right decrement pyramid
+# count=int(input("enter row count="))
+# for i in range(count):
+#     for k in range(i):
+#         print(end=" ")
+
+#     for j in range(count,i,-1):
+#         print("*",end="")
+#     print()
+
+
+#hill-increment pyramid
+# count=int(input("enter row count="))
+# for i in range(count):
+#     for k in range(count,i+1,-1):
+#         print(end=" ")
+
+#     for j in range(i+1):
+#         print("* ",end="")
+#     print()
+
+# #hill decrement pyramid
+# count=int(input("enter row count="))
+# for i in range(count):
+#     for k in range(i):
+#         print(end=" ")
+
+#     for j in range(count,i,-1):
+#         print("* ",end="")
+#     print()
+
+
---
+++ python09/03/tupledemo.py
@@ -0,0 +1,13 @@
+#tuple->collecting different data elements enclosed into () paranthesis
+#it is seperated,index,follow,referred immutable mans can't do curd operation
+
+#create tuple
+tup=()
+print(tup,type(tup))
+t1=tuple()
+print(t1,type(t1))
+
+t2=(500)
+print(t2,type(t1))
+
+t3=
